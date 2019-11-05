ff814078bba4f49cd2cdb29d3a74871a7f88c460
Yang Liu <liuyang12@focusmedia.cn>
2019-11-05 16:03:25 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -2,6 +2,7 @@ import pickle
 
 import numpy as np
 from torch.utils.data import Dataset
+from config import im_size
 
 
 class DeepHNDataset(Dataset):
@@ -15,7 +16,7 @@ class DeepHNDataset(Dataset):
     def __getitem__(self, i):
         sample = self.samples[i]
         image, H_four_points = sample
-        img = np.zeros((128, 128, 3), np.float32)
+        img = np.zeros((im_size, im_size, 3), np.float32)
         img[:, :, 0:2] = image / 255.
         img = np.transpose(img, (2, 0, 1))  # HxWxC array to CxHxW
         target = np.reshape(H_four_points, (8,))
