3efbb1bd9c3fa08c07d0bbe1489b21717effb412
Yang Liu <liuyang12@focusmedia.cn>
2019-11-06 17:20:48 +0800

    update

--- test/test_gen_data.py
+++ test/test_gen_data.py
@@ -3,12 +3,12 @@ import cv2 as cv
 import numpy as np
 
 from config import rho, four_points, top_point, bottom_point
-# from numpy.linalg import inv
+from numpy.linalg import inv
 from test_orb import compute_homo
 
 if __name__ == "__main__":
-    np.random.seed(7)
-    random.seed(7)
+    # np.random.seed(7)
+    # random.seed(7)
     fullpath = '000000523955.jpg'
     img = cv.imread(fullpath, 0)
     img = cv.resize(img, (640, 480))
@@ -20,16 +20,16 @@ if __name__ == "__main__":
 
     H = cv.getPerspectiveTransform(np.float32(four_points), np.float32(perturbed_four_points))
     print(H)
-    # H_inverse = inv(H)
+    H_inverse = inv(H)
 
-    # warped_image = cv.warpPerspective(img, H_inverse, (640, 480))
-    warped_image = cv.warpPerspective(img, H, (640, 480))
+    warped_image = cv.warpPerspective(img, H_inverse, (640, 480))
+    # warped_image = cv.warpPerspective(img, H, (640, 480))
 
     Ip1 = test_image[top_point[1]:bottom_point[1], top_point[0]:bottom_point[0]]
     Ip2 = warped_image[top_point[1]:bottom_point[1], top_point[0]:bottom_point[0]]
 
-    # test_image = cv.polylines(test_image, [np.int32(four_points)], True, 255, 3, cv.LINE_AA)
-    # warped_image = cv.polylines(warped_image, [np.int32(perturbed_four_points)], True, 255, 3, cv.LINE_AA)
+    warped_image = cv.polylines(warped_image, [np.int32(four_points)], True, 255, 3, cv.LINE_AA)
+    test_image = cv.polylines(test_image, [np.int32(perturbed_four_points)], True, 255, 3, cv.LINE_AA)
 
     Ip1_new = np.zeros((320, 320), np.uint8)
     Ip1_new[64:, 64:] = Ip1
