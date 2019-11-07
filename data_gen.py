93c8c2a0f063922472621e04660754d2013a4a07
Yang Liu <liuyang12@focusmedia.cn>
2019-11-07 13:54:02 +0800

    udpate

--- data_gen.py
+++ data_gen.py
@@ -1,5 +1,6 @@
 import pickle
 
+import cv2 as cv
 import numpy as np
 from torch.utils.data import Dataset
 
@@ -13,13 +14,19 @@ class DeepHNDataset(Dataset):
         with open(filename, 'rb') as file:
             samples = pickle.load(file)
         np.random.shuffle(samples)
+        self.split = split
         self.samples = samples
 
     def __getitem__(self, i):
         sample = self.samples[i]
         image, four_points, perturbed_four_points = sample
+        img0 = image[:, :, 0]
+        img0 = cv.resize(img0, (im_size, im_size))
+        img1 = image[:, :, 1]
+        img1 = cv.resize(img1, (im_size, im_size))
         img = np.zeros((im_size, im_size, 3), np.float32)
-        img[:, :, 0:2] = image / 255.
+        img[:, :, 0] = img0 / 255.
+        img[:, :, 1] = img1 / 255.
         img = np.transpose(img, (2, 0, 1))  # HxWxC array to CxHxW
         H_four_points = np.subtract(np.array(perturbed_four_points), np.array(four_points))
         target = np.reshape(H_four_points, (8,))
