4236afc447da596fb2a93a8c8f425090c7ab352d
Yang Liu <liuyang12@focusmedia.cn>
2019-11-07 15:50:22 +0800

    update

--- test_orb.py
+++ test_orb.py
@@ -10,37 +10,37 @@ MIN_MATCH_COUNT = 10
 
 
 def compute_homo(img1, img2):
-    try:
-        # Initiate SIFT detector
-        # sift = cv2.xfeatures2d.SIFT_create()
-        sift = cv2.xfeatures2d.SURF_create()
-
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1, None)
-        kp2, des2 = sift.detectAndCompute(img2, None)
-
-        FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
-
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-        matches = flann.knnMatch(des1, des2, k=2)
-
-        # store all the good matches as per Lowe's ratio test.
-        good = []
-        for m, n in matches:
-            if m.distance < 0.7 * n.distance:
-                good.append(m)
-
-        if len(good) > MIN_MATCH_COUNT:
-            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-            H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            return H
-    except cv2.error as err:
-        print(err)
+    # try:
+    #     # Initiate SIFT detector
+    #     # sift = cv2.xfeatures2d.SIFT_create()
+    #     sift = cv2.xfeatures2d.SURF_create()
+    #
+    #     # find the keypoints and descriptors with SIFT
+    #     kp1, des1 = sift.detectAndCompute(img1, None)
+    #     kp2, des2 = sift.detectAndCompute(img2, None)
+    #
+    #     FLANN_INDEX_KDTREE = 0
+    #     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    #     search_params = dict(checks=50)
+    #
+    #     flann = cv2.FlannBasedMatcher(index_params, search_params)
+    #
+    #     matches = flann.knnMatch(des1, des2, k=2)
+    #
+    #     # store all the good matches as per Lowe's ratio test.
+    #     good = []
+    #     for m, n in matches:
+    #         if m.distance < 0.7 * n.distance:
+    #             good.append(m)
+    #
+    #     if len(good) > MIN_MATCH_COUNT:
+    #         src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    #         dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+    #
+    #         H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    #         return H
+    # except cv2.error as err:
+    #     print(err)
     return np.identity(3)
 
 
