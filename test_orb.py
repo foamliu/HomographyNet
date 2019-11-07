26b5340f20e34456904800890c61dda2100a47f0
Yang Liu <liuyang12@focusmedia.cn>
2019-11-07 14:54:42 +0800

    update

--- test_orb.py
+++ test_orb.py
@@ -60,7 +60,7 @@ def compute_mse(H, four_points, perturbed_four_points):
     # print('error: ' + str(error))
     # print('H_four_points: ' + str(H_four_points))
     H_four_points = np.subtract(perturbed_four_points, four_points)
-    mse = (np.square(diff - H_four_points)).mean()
+    mse = (np.abs(diff - H_four_points)).mean()
     return mse
 
 
