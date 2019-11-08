64cf76be463559a9850380a8ad834368e1a4878b
Yang Liu <liuyang12@focusmedia.cn>
2019-11-08 09:05:18 +0800

    update

--- test_orb.py
+++ test_orb.py
@@ -1,5 +1,5 @@
 import pickle
-
+import argparse
 import cv2
 import numpy as np
 
@@ -9,39 +9,45 @@ from utils import AverageMeter
 MIN_MATCH_COUNT = 10
 
 
-def compute_homo(img1, img2):
-    try:
-        # Initiate SIFT detector
-        # sift = cv2.xfeatures2d.SIFT_create()
-        sift = cv2.xfeatures2d.SURF_create()
+def compute_homo(img1, img2, args):
+    H = np.identity(3)
+    if args.type == 'surf':
+        try:
+            # Initiate SIFT detector
+            # sift = cv2.xfeatures2d.SIFT_create()
+            sift = cv2.xfeatures2d.SURF_create()
 
-        # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img1, None)
-        kp2, des2 = sift.detectAndCompute(img2, None)
+            # find the keypoints and descriptors with SIFT
+            kp1, des1 = sift.detectAndCompute(img1, None)
+            kp2, des2 = sift.detectAndCompute(img2, None)
 
-        FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
+            FLANN_INDEX_KDTREE = 0
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)
 
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
+            flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-        matches = flann.knnMatch(des1, des2, k=2)
+            matches = flann.knnMatch(des1, des2, k=2)
 
-        # store all the good matches as per Lowe's ratio test.
-        good = []
-        for m, n in matches:
-            if m.distance < 0.7 * n.distance:
-                good.append(m)
+            # store all the good matches as per Lowe's ratio test.
+            good = []
+            for m, n in matches:
+                if m.distance < 0.7 * n.distance:
+                    good.append(m)
 
-        if len(good) > MIN_MATCH_COUNT:
-            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            if len(good) > MIN_MATCH_COUNT:
+                src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+                dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
-            H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            return H
-    except cv2.error as err:
-        print(err)
-    return np.identity(3)
+                H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+         except cv2.error as err:
+            print(err)
+
+    elif args.type == 'identity':
+        pass
+
+    return H
 
 
 def compute_mace(H, perturbed_four_points):
@@ -60,7 +66,7 @@ def compute_mace(H, perturbed_four_points):
     return mace
 
 
-def test():
+def test(args):
     filename = 'data/test.pkl'
     with open(filename, 'rb') as file:
         samples = pickle.load(file)
@@ -74,7 +80,7 @@ def test():
         img2 = np.zeros((640, 480), np.uint8)
         img2[64:320, 64:320] = image[:, :, 1]
 
-        H = compute_homo(img2, img1)
+        H = compute_homo(img2, img1, args)
         try:
             mace = compute_mace(H, perturbed_four_points)
             mace_list.append(mace)
@@ -82,11 +88,21 @@ def test():
         except cv2.error as err:
             print(err)
         if i % print_freq == 0:
-            print('[{0}/{1}]\tMean Average Corner Error {mace.val:.5f} ({mace.avg:.5f})'.format(i, len(samples), mace=maces))
+            print('[{0}/{1}]\tMean Average Corner Error {mace.val:.5f} ({mace.avg:.5f})'.format(i, len(samples),
+                                                                                                mace=maces))
 
     print('MSE: {:5f}'.format(np.mean(mace_list)))
     print('len(mse_list): ' + str(len(mace_list)))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Test with SURF+RANSAC')
+    # general
+    parser.add_argument('--type', type=str, default='surf', help='surf or identity')
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
-    test()
+    args = parse_args()
+    test(args)
